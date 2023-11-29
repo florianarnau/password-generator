843e2d96b3f4aea69a9dbcfd319a7ede31767784
Florian ARNAU <arnauflorian@outlook.com>
2023-11-29 20:40:27 +0100

    feat: ajout de contrôle de saisi, .gitignore

--- src/main/java/passwordgenerator/PasswordGenerator.java
+++ src/main/java/passwordgenerator/PasswordGenerator.java
@@ -10,30 +10,69 @@ import java.util.Scanner;
  */
 public class PasswordGenerator {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Entrez la longueur du mot de passe : ");
-        int length = scanner.nextInt();
+        int length;
+        do {
+            System.out.println("Entrez la longueur du mot de passe (minimum 8 caractères) : ");
+            String input = scanner.nextLine().trim();
 
-        System.out.println("Voulez-vous inclure des lettres majuscules ? (Oui/Non) : ");
-        boolean includeUpperCase = scanner.next().equalsIgnoreCase("Oui");
+            if (input.isEmpty()) {
+                length = 8; // Longueur par défaut si l'utilisateur appuie sur "Entrée".
+                break;
+            }
 
-        System.out.println("Voulez-vous inclure des lettres minuscules ? (Oui/Non) : ");
-        boolean includeLowerCase = scanner.next().equalsIgnoreCase("Oui");
-    
-        System.out.print("Voulez-vous inclure des chiffres ? (Oui/Non) : ");
-        boolean includeDigits = scanner.next().equalsIgnoreCase("Oui");
+            try {
+                length = Integer.parseInt(input);
+                if (length < 8) {
+                    System.out.println("La longueur du mot de passe doit être au minimum égale à 8.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Veuillez entrer un nombre valide.");
+                length = -1;
+            }
+        } while (length < 8);
 
-        System.out.print("Voulez-vous inclure des caractères spéciaux ? (Oui/Non) : ");
-        boolean includeSpecialChars = scanner.next().equalsIgnoreCase("Oui");
-        
+        boolean includeUpperCase, includeLowerCase, includeDigits, includeSpecialChars;
+
+        do {
+            includeUpperCase = getBooleanInput("Voulez-vous inclure des lettres majuscules ? (Oui/Non) : ", scanner);
+            includeLowerCase = getBooleanInput("Voulez-vous inclure des lettres minuscules ? (Oui/Non) : ", scanner);
+            includeDigits = getBooleanInput("Voulez-vous inclure des chiffres ? (Oui/Non) : ", scanner);
+            includeSpecialChars = getBooleanInput("Voulez-vous inclure des caractères spéciaux ? (Oui/Non) : ", scanner);
+
+            // Vérifie que l'utilisateur a répondu "Oui" à au moins une des options
+            if (!(includeUpperCase || includeLowerCase || includeDigits || includeSpecialChars)) {
+                System.out.println("Vous devez inclure au moins un type de caractère. Veuillez répondre par 'Oui' pour au moins une option.");
+            }
+        } while (!(includeUpperCase || includeLowerCase || includeDigits || includeSpecialChars));
+
         String password = generatePassword(length, includeUpperCase, includeLowerCase, includeDigits, includeSpecialChars);
 
         System.out.println("Mot de passe généré : " + password);
 
         savePasswordToFile("- " + password);
     }
+
+    private static boolean getBooleanInput(String message, Scanner scanner) {
+        boolean result;
+        String userInput;
+        do {
+            System.out.print(message);
+            userInput = scanner.nextLine().trim().toLowerCase();
+            if (userInput.equals("oui")) {
+                result = true;
+            } else if (userInput.equals("non")) {
+                result = false;
+            } else {
+                System.out.println("Veuillez répondre par 'Oui' ou 'Non'.");
+                result = false;
+            }
+        } while (!userInput.equals("oui") && !userInput.equals("non"));
+        return result;
+    }
+
 
     private static void savePasswordToFile(String password) {
         try (FileWriter writer = new FileWriter("passwords.txt", true)) {
@@ -51,6 +90,11 @@ public class PasswordGenerator {
         String lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
         String digitChars = "0123456789";
         String specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?";
+
+        if (!includeUpperCase && !includeLowerCase && !includeDigits && !includeSpecialChars) {
+            System.out.println("Vous devez inclure au moins un type de caractère. Veuillez répondre par 'Oui' pour au moins une option.");
+            return ""; // Retourne un mot de passe vide
+        }
 
         String allChars = "";
 
